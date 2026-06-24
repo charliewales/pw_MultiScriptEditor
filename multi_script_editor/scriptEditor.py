8130d9e1c8a9a78f8e650fd8fa1a44416dde7ce8
CW-AMD\cw <carlos.rico.3d@gmail.com>
2026-06-24 13:29:11 +0200

    chore: bump patch version

--- multi_script_editor/scriptEditor.py
+++ multi_script_editor/scriptEditor.py
@@ -10,7 +10,7 @@ if not os.environ.get("QT_PREFERRED_BINDING"):
 # Disable High Dpi Scaling in PySide6
 os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
 
-mse_version = "5.1.0"
+mse_version = "5.1.1"
 
 import managers
 import sessionManager
