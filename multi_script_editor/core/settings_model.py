d2d569276543d5afee4ac5c1e991c8cf4c72c08f
CW-AMD\cw <carlos.rico.3d@gmail.com>
2026-07-08 18:02:23 +0200

    chore: remove some default snippets

--- multi_script_editor/core/settings_model.py
+++ multi_script_editor/core/settings_model.py
@@ -137,7 +137,5 @@ class SnippetsModel(SettingsModel):
         return dict(snippets={
             "Python: Main block": "if __name__ == '__main__':\n    # Main code here\n    pass",
             "Python: Class Template": "class MyClass(object):\n    def __init__(self):\n        super(MyClass, self).__init__()\n        pass",
-            "Python: Try/Except": "import traceback\ntry:\n    pass\nexcept Exception as e:\n    print(f\"Error: {e}\")\n    traceback.print_exc()",
-            "Python: If/Else": "if condition:\n    pass\nelse:\n    pass",
             "Qt: Basic Window": "from PySide2.QtWidgets import QMainWindow, QApplication\nimport sys\n\nclass MyWindow(QMainWindow):\n    def __init__(self, parent=None):\n        super(MyWindow, self).__init__(parent)\n        self.setWindowTitle(\"My UI\")\n        self.resize(400, 300)\n\nif __name__ == '__main__':\n    app = QApplication(sys.argv)\n    win = MyWindow()\n    win.show()\n    sys.exit(app.exec_())"
         })
